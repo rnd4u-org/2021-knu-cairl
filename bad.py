aca5f87c3b0c6500d814d68b4d1be1e6f23187f8
meanalexrin <58822594+meanalexrin@users.noreply.github.com>
2021-01-14 20:07:23 +0200

    Create bad.py

---
+++ bad.py
@@ -0,0 +1,3 @@
+BadStandards = "lllllllllllllllllllllllllllllllllllllooooooooooooooooooooooooooooooooooooooooooooooooooooooonnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnggggggggggggggggggggggggggg"
+
+import wat
