c703e182e7808126e8d67e218a3fb1c8d1d298fe
Vova Gusev <gusevvovik@gmail.com>
2021-01-29 18:22:28 +0200

    House prices after pep8

--- tasks/task_2/house_prices.py
+++ tasks/task_2/house_prices.py
@@ -15,7 +15,6 @@ import pandas as pd
 import seaborn as sns
 import xgboost as xgb
 import scipy.stats as stats
-from scipy.stats import norm
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 from scipy.stats import skew, norm
@@ -28,12 +27,10 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeRegressor
 from mlxtend.regressor import StackingRegressor
 from sklearn.linear_model import LinearRegression, BayesianRidge
-from sklearn.model_selection import RepeatedKFold
 from sklearn.model_selection import KFold, cross_val_score
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
-from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
-from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_squared_log_error
+from sklearn.metrics import mean_squared_error
 pd.options.mode.chained_assignment = None
 # %matplotlib inline
 warnings.filterwarnings('ignore')
@@ -52,14 +49,18 @@ print(test_data.isna().sum())
 
 """**Let's explore our target variable and how the other features influence it.**"""
 
+pip install flake8 pycodestyle_magic
+
+# Commented out IPython magic to ensure Python compatibility.
+# %load_ext pycodestyle_magic
+
 (mu, sigma) = norm.fit(train_data['SalePrice'])
 
-plt.figure(figsize = (12,6))
+plt.figure(figsize=(12, 6))
 sns.distplot(train_data['SalePrice'], kde=True, hist=True, fit=norm)
 plt.title('SalePrice distribution vs Normal Distribution', fontsize=13)
 plt.xlabel("House's sale Price in $", fontsize=12)
-plt.legend(['Normal dist. ($\mu=$ {:.2f} and $\sigma=$ {:.2f} )'.format(mu, sigma)],
-            loc='best')
+plt.legend([r'Normal dist. ($\mu=$ {:.2f} and $\sigma=$ {:.2f} )'.format(mu, sigma)], loc='best')
 plt.show()
 
 """**The distribution does not seem to be normal, but highly right-skewed.**"""
@@ -93,22 +94,22 @@ Train and Test sets, preprocess, and divide them again.
 
 target = train_data['SalePrice']
 test_id = test_data['Id']
-test = test_data.drop(['Id'],axis=1)
-train = train_data.drop(['SalePrice','Id'], axis=1)
-train_test = pd.concat([train,test], axis=0, sort=False)
+test = test_data.drop(['Id'], axis=1)
+train = train_data.drop(['SalePrice', 'Id'], axis=1)
+train_test = pd.concat([train, test], axis=0, sort=False)
 
 """Let's check the amount of missed(Nan) values for each variable"""
 
 missed = pd.DataFrame(train_test.isna().sum(), columns=['Amount'])
 missed['feat'] = missed.index
-missed['Perc(%)'] = (missed['Amount']/1460)*100
+missed['Perc(%)'] = (missed['Amount'] / 1460) * 100
 missed = missed[missed['Amount'] > 0]
 missed = missed.sort_values(by=['Amount'])
 missed
 
 """Plotting missed(Nan)"""
 
-plt.figure(figsize=(15,5))
+plt.figure(figsize=(15, 5))
 sns.barplot(x=missed['feat'], y=missed['Perc(%)'])
 plt.xticks(rotation=45)
 plt.title('Features containing Nan')
@@ -147,8 +148,8 @@ train_test['MoSold'] = train_test['MoSold'].apply(str)
 
 """Removing the useless variables"""
 
-useless = ['GarageYrBlt','YearRemodAdd'] 
-train_test = train_test.drop(useless, axis = 1)
+useless = ['GarageYrBlt', 'YearRemodAdd'] 
+train_test = train_test.drop(useless, axis=1)
 
 # Imputing with KnnRegressor 
 
@@ -166,8 +167,8 @@ def impute_knn(df):
         model = KNeighborsRegressor(n_neighbors=5)
         knr = model.fit(imp_train[cols_no_nan], imp_train[col])
         ttn.loc[ttn[col].isna(), col] = knr.predict(imp_test[cols_no_nan])
-    
-    return pd.concat([ttn,ttc],axis=1)
+
+    return pd.concat([ttn, ttc], axis=1)
 
 
 train_test = impute_knn(train_test)
@@ -215,10 +216,10 @@ for i in skew_index:
 
 # SalePrice before transformation
 
-fig, ax = plt.subplots(1,2, figsize=(15,5))
+fig, ax = plt.subplots(1, 2, figsize=(15, 5))
 fig.suptitle(" qq-plot & distribution SalePrice ", fontsize=15)
 
-sm.qqplot(target, stats.t, distargs=(4,),fit=True, line="45", ax=ax[0])
+sm.qqplot(target, stats.t, distargs=(4,), fit=True, line="45", ax=ax[0])
 
 sns.distplot(target, kde=True, hist=True, fit=norm, ax=ax[1])
 plt.show()
@@ -227,11 +228,11 @@ plt.show()
 
 target_log = np.log1p(target)
 
-fig, ax = plt.subplots(1,2, figsize=(15,5))
-fig.suptitle("qq-plot & distribution SalePrice ", fontsize= 15)
+fig, ax = plt.subplots(1, 2, figsize=(15, 5))
+fig.suptitle("qq-plot & distribution SalePrice ", fontsize=15)
 
-sm.qqplot(target_log, stats.t, distargs=(4,),fit=True, line="45", ax=ax[0])
-sns.distplot(target_log, kde = True, hist=True, fit=norm, ax=ax[1])
+sm.qqplot(target_log, stats.t, distargs=(4,), fit=True, line="45", ax=ax[0])
+sns.distplot(target_log, kde=True, hist=True, fit=norm, ax=ax[1])
 plt.show()
 
 """**Modeling**"""
@@ -243,7 +244,7 @@ test = train_test_dummy[1460:]
 test['Id'] = test_id
 
 # Creation of the RMSE metric:
-  
+
 
 def rmse(y, y_pred):
     return np.sqrt(mean_squared_error(y, y_pred))
@@ -261,9 +262,9 @@ kf = KFold(n_splits=10, random_state=42, shuffle=True)
 cv_scores = []
 cv_std = []
 
-baseline_models = ['Linear_Reg.','Bayesian_Ridge_Reg.','LGBM_Reg.','SVR',
-                   'Dec_Tree_Reg.','Random_Forest_Reg.', 'XGB_Reg.',
-                   'Grad_Boost_Reg.','Cat_Boost_Reg.','Stacked_Reg.']
+baseline_models = ['Linear_Reg.', 'Bayesian_Ridge_Reg.', 'LGBM_Reg.', 'SVR',
+                   'Dec_Tree_Reg.', 'Random_Forest_Reg.', 'XGB_Reg.',
+                   'Grad_Boost_Reg.', 'Cat_Boost_Reg.', 'Stacked_Reg.']
 
 # Linear Regression
 
@@ -347,8 +348,8 @@ final_cv_score['RMSE_std'] = cv_std
 
 final_cv_score
 
-plt.figure(figsize=(12,8))
-sns.barplot(final_cv_score['Regressors'],final_cv_score['RMSE_mean'])
+plt.figure(figsize=(12, 8))
+sns.barplot(final_cv_score['Regressors'], final_cv_score['RMSE_mean'])
 plt.xlabel('Regressors', fontsize=12)
 plt.ylabel('CV_Mean_RMSE', fontsize=12)
 plt.xticks(rotation=45)
@@ -359,7 +360,7 @@ plt.show()
 So we will use it for predicting results
 """
 
-X_train,X_val,y_train,y_val = train_test_split(train,target_log,test_size=0.1,random_state=42)
+X_train, X_val, y_train, y_val = train_test_split(train, target_log, test_size=0.1, random_state=42)
 
 # Cat Boost Regressor
 
@@ -367,16 +368,16 @@ params = {'iterations': 6000,
           'learning_rate': 0.005,
           'depth': 4,
           'l2_leaf_reg': 1,
-          'eval_metric':'RMSE',
+          'eval_metric': 'RMSE',
           'early_stopping_rounds': 200,
           'verbose': 200,
           'random_seed': 42}
-         
+
 cat = CatBoostRegressor(**params)
-cat_model = cat_f.fit(X_train,y_train,
-                     eval_set=(X_val,y_val),
-                     plot=True,
-                     verbose=False)
+cat_model = cat_f.fit(X_train, y_train,
+                      eval_set=(X_val, y_val),
+                      plot=True,
+                      verbose=False)
 
 cat_pred = cat_model_f.predict(X_val)
 cat_score = rmse(y_val, catf_pred)
