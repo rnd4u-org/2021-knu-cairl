7097002af352a2bb73ec0b903b19ad70d9433ca2
TatianaBelyukina <tasyabelyukina@gmail.com>
2021-02-01 19:16:25 +0200

    Add files via upload

--- TatianaBelyukina_code/task2/pre.py
+++ TatianaBelyukina_code/task2/pre.py
@@ -5,37 +5,40 @@ def splitColumn(data, feature):
         for i in range(data.shape[0]):
             if data[feature][i] == val:
                 a.append(1)
-            else: a.append(0)
-        data[feature+"_"+str(val)] = a
-    return data.drop([feature], axis = 1)
+            else:
+                a.append(0)
+        data[feature+ "_" +str(val)] = a
+    return data.drop([feature], axis=1)
+
 
 def splitFloatColumn(data, feature, diapasones):
-    for i in range(len(diapasones)+1):
+    for i in range(len(diapasones) + 1):
         a = []
         for j in range(data.shape[0]):
             t = True
             if i > 0:
-                t &= (data[feature][j] >= diapasones[i-1])
+                t &= (data[feature][j] >= diapasones[i - 1])
             if i < len(diapasones):
                 t &= (data[feature][j] < diapasones[i])
             if t:
                 a.append(1)
-            else: a.append(0)
-        data[feature+"_"+str(i)] = a
-    return data.drop([feature], axis = 1)
+            else:
+                a.append(0)
+        data[feature + "_" + str(i)] = a
+    return data.drop([feature], axis=1)
 
 
 def prepare(data):
     # for val in data:
     #     if val in ['Alley', 'PoolQC', 'Fence', 'MiscFeature']:
     #       print(val)
-    data = data.drop(['Alley', 'PoolQC', 'Fence', 'MiscFeature', "Id"], axis = 1)
+    data = data.drop(['Alley', 'PoolQC', 'Fence', 'MiscFeature', "Id"], axis=1)
 
     for val in data:
         if data[val].dtypes == 'object':
-            data[val]=data[val].fillna(data[val].mode()[0])
-        else: data[val]=data[val].fillna(data[val].mean())
-
+            data[val] = data[val].fillna(data[val].mode()[0])
+        else: 
+            data[val] = data[val].fillna(data[val].mean())
 
     for val in data:
         if data[val].dtypes == 'object':
@@ -50,6 +53,7 @@ def fit_set(data, example):
             data[val] = [0] * data.shape[0]
     return data
 
+
 def split_data(data):
     X = data.drop(['SalePrice'], axis=1)
     Y = data['SalePrice']
@@ -57,4 +61,3 @@ def split_data(data):
     from sklearn.model_selection import train_test_split
 
     return train_test_split(X, Y, test_size=0.2)
- 
