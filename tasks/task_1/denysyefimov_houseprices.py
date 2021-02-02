1c202439936043742cc8789c4f29d26aa11ff7b2
Detroiman <52424452+Detroiman@users.noreply.github.com>
2021-02-02 10:04:14 +0200

    Add files via upload
    Removed all references to my local directory and removed code not used in .py extension

--- tasks/task_1/denysyefimov_houseprices.py
+++ tasks/task_1/denysyefimov_houseprices.py
@@ -28,7 +28,8 @@ if __name__ == '__main__':
 
     deviation = np.log(train.SalePrice)
 
-    # Now let's look at what influences the price of the house itself and analyze the data.
+    # Now let's look at what influences
+    # the price of the house itself and analyze the data.
 
     influences = train.select_dtypes(include=[np.number])
     influences.dtypes
@@ -36,15 +37,24 @@ if __name__ == '__main__':
     print(percent['SalePrice'].sort_values(ascending=False)[:5], '\n')
     print(percent['SalePrice'].sort_values(ascending=False)[-5:])
     quality_pivot = train.pivot_table(index='OverallQual',
-                                      values='SalePrice', aggfunc=np.median)
+                                      values='SalePrice',
+                                      aggfunc=np.median
+                                      )
     print(quality_pivot)
 
-    # Thus, we got that the price is most affected by: OverallQual, GrLivArea, GarageCars,
-    # GarageArea, YrSold, OverallCond, MSSubClass, EnclosedPorch, KitchenAbvGr.
-    # Now let's see the rest of the factors that affect the price.
-    # Thus, we got the graphs and now it would be worth removing the points that could
-    # shift our line from the "center of events".
-    # Let's set the maximum value of the garage area to 1200, so that the line doesn't run away from us.
+    # Thus, we got that the price is most
+    # affected by: OverallQual, GrLivArea, GarageCars,
+    # GarageArea, YrSold, OverallCond,
+    # MSSubClass, EnclosedPorch, KitchenAbvGr.
+    # Now let's see the rest of the
+    # factors that affect the price.
+    # Thus, we got the graphs and now
+    # it would be worth removing the points that could
+    # shift our line from the "center
+    # of events".
+    # Let's set the maximum value of
+    # the garage area to 1200, so that the line
+    # doesn't run away from us.
     # And makes some changes in GrLivArea
 
     train = train[train['GarageArea'] < 1200]
@@ -62,8 +72,12 @@ if __name__ == '__main__':
 
     # Let's use one-hot encoding method for our non-numeric data
 
-    train['enc_street'] = pd.get_dummies(train.Street, drop_first=True)
-    test['enc_street'] = pd.get_dummies(train.Street, drop_first=True)
+    train['enc_street'] = pd.get_dummies(train.Street,
+                                         drop_first=True
+                                         )
+    test['enc_street'] = pd.get_dummies(train.Street,
+                                        drop_first=True
+                                        )
 
     train['enc_condition'] = train.SaleCondition.apply(one_hot_encode)
     test['enc_condition'] = test.SaleCondition.apply(one_hot_encode)
@@ -72,40 +86,57 @@ if __name__ == '__main__':
 
     data = train.select_dtypes(include=[np.number]).interpolate().dropna()
 
-    # Thus, we redid the reasons for the sale and got a simpler model, which is easier to work with both for us
-    # and the computer. And also, those data that we do not use have been converted to zero, also in order to
+    # Thus, we redid the reasons for the sale and
+    # got a simpler model, which is easier to work
+    # with both for us
+    # and the computer. And also, those data that
+    # we do not use have been converted to zero,
+    # also in order to
     # make it easier for us to work.
     # Now let's start building our linear model.
 
     y = np.log(train.SalePrice)
     X = data.drop(['SalePrice', 'Id'], axis=1)
-    X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                        random_state=100, test_size=.33)
+    X_train, X_test, y_train, y_test = train_test_split(X,
+                                                        y,
+                                                        random_state=100,
+                                                        test_size=.33
+                                                        )
     lr = linear_model.LinearRegression()
     model = lr.fit(X_train, y_train)
     predictions = model.predict(X_test)
     actual_values = y_test
 
     # Now let's build our line with a predicted price.
-    # Now let's change our alpha a little and get some graphics. With alpha = 1 we get better prediction.
+    # Now let's change our alpha a little and get some
+    # graphics. With alpha = 1 we get better prediction.
 
     alpha = 1
     rm = linear_model.Ridge(alpha=alpha)
     ridge_model = rm.fit(X_train, y_train)
     preds_ridge = ridge_model.predict(X_test)
-    plt.scatter(preds_ridge, actual_values, alpha=.75, color='black')
+    plt.scatter(preds_ridge,
+                actual_values,
+                alpha=.75,
+                color='black'
+                )
     plt.xlabel('Predicted Price')
     plt.ylabel('Actual Price')
     plt.title('Ridge Regularization with alpha = {}'.format(alpha))
     overlay = 'R^2 is: {}\nRMSE is: {}'.format(ridge_model.score(X_test, y_test),
                                                mean_squared_error(y_test, preds_ridge))
-    plt.annotate(s=overlay, xy=(12.1, 10.6), size='x-large')
+    plt.annotate(s=overlay,
+                 xy=(12.1, 10.6),
+                 size='x-large'
+                 )
 
     # Finally, we get our final answer.
 
     submission = pd.DataFrame()
     submission['Id'] = test.Id
-    feats = test.select_dtypes(include=[np.number]).drop(['Id'], axis=1).interpolate()
+    feats = test.select_dtypes(include=[np.number]).drop(['Id'],
+                                                         axis=1
+                                                         ).interpolate()
     predictions = ridge_model.predict(feats)
     final_predictions = np.exp(predictions)
     print("Final predictions are: \n", final_predictions[:5])
