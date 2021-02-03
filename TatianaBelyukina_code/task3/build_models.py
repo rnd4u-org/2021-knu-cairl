95fbda43b064f741333ac7b8ba1139fc7ad77caf
TatianaBelyukina <tasyabelyukina@gmail.com>
2021-02-03 23:18:56 +0200

    Add files via upload

---
+++ TatianaBelyukina_code/task3/build_models.py
@@ -0,0 +1,49 @@
+def build_model():
+    from keras.models import Sequential
+    from keras.layers import Conv2D,MaxPooling2D,\
+        Dropout,Flatten,Dense,Activation,\
+        BatchNormalization
+
+    model=Sequential()
+
+    model.add(Conv2D(32,(3,3),activation='relu',input_shape=(224,224,3)))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(64,(3,3),activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(128,(3,3),activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(512,activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
+    model.add(Dense(2,activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy',
+        optimizer='rmsprop',metrics=['accuracy'])
+    return model
+
+def build_model_transfer():
+    from tensorflow.keras.applications.inception_v3 import InceptionV3
+    from tensorflow.keras.models import Model
+    from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+
+    base_model = InceptionV3(weights='imagenet', include_top=False)
+
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    
+    x = Dense(1024, activation='relu')(x)
+    x = Dense(512, activation='relu')(x)
+    x = Dense(128, activation='relu')(x)
+    predictions = Dense(2, activation='softmax')(x)
+
+    return Model(inputs=base_model.input, outputs=predictions)
