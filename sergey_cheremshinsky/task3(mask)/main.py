5554256d8189effb758d3831ddbcb1b14621f778
sergTch <42450404+sergTch@users.noreply.github.com>
2021-02-03 23:54:17 +0200

    Add files via upload

--- sergey_cheremshinsky/task3(mask)/main.py
+++ sergey_cheremshinsky/task3(mask)/main.py
@@ -1,4 +1,3 @@
-import tensorflow as tf
 from load_data import load_data
 from model import build_model
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau
@@ -11,13 +10,13 @@ if __name__ == "__main__":
     steps = 3
     batch = 10
 
-    earlystop = EarlyStopping(patience = 3, monitor='accuracy')
-    learning_rate_reduction = ReduceLROnPlateau(monitor = 'accuracy', patience = 2, verbose = 1, factor = 0.5, min_lr = 0.0001)
+    earlystop = EarlyStopping(patience=3, monitor='accuracy')
+    learning_rate_reduction = ReduceLROnPlateau(monitor='accuracy', patience=2, verbose=1, factor=0.5, min_lr=0.0001)
     callbacks = [earlystop, learning_rate_reduction]
 
     for step in range(2, steps - 1):
         for i in range(size):
-            model.layers[i].trainable = size - size/steps * step > i and size - size / steps * (step + 1) >= i
+            model.layers[i].trainable = size - size / steps * step > i and size - size / steps * (step + 1) >= i
             model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
             history = model.fit(
                 x=x_train, 
@@ -31,7 +30,7 @@ if __name__ == "__main__":
 
     for i in range(size):
         model.layers[i].trainable = size - size / steps * (steps - 1) > i
-        
+
     model.compile(optimizer=SGD(lr=0.001, momentum=0.9), loss='categorical_crossentropy', metrics=['accuracy'])
 
     history = model.fit(
@@ -42,7 +41,6 @@ if __name__ == "__main__":
         validation_steps=1511 // batch,
         steps_per_epoch=6042 // batch,
         callbacks=callbacks)
-
 
     model.save("final_model")
     print(history)
