32596d3f1c8a05f8ee7c70f9a9c882e042934fc9
Detroiman <52424452+Detroiman@users.noreply.github.com>
2021-02-08 14:11:19 +0200

    Update denysyefimov_face_recogn.py

--- denysyefimov_code/task4_face_recognition/denysyefimov_face_recogn.py
+++ denysyefimov_code/task4_face_recognition/denysyefimov_face_recogn.py
@@ -29,7 +29,7 @@ def prewhiten(x):
 
     mean = np.mean(x, axis=axis, keepdims=True)
     std = np.std(x, axis=axis, keepdims=True)
-    std_adj = np.maximum(std, 1.0/np.sqrt(size))
+    std_adj = np.maximum(std, 1.0  /np.sqrt(size))
     y = (x - mean) / std_adj
     return y
 
@@ -53,8 +53,8 @@ def load_and_align_images(filepaths, margin):
                                          minNeighbors=3)
         (x, y, w, h) = faces[0]
         print(faces[0])
-        cropped = img[y-margin//2:y+h+margin//2,
-                      x-margin//2:x+w+margin//2, :]
+        cropped = img[y  -margin // 2: y + h + margin // 2,
+                      x  -margin / /2: x + w + margin // 2, :]
         aligned = resize(cropped, (image_size, image_size), mode='reflect')
         aligned_images.append(aligned)
     return np.array(aligned_images)
@@ -65,7 +65,7 @@ def calc_embs(filepaths, margin=10, batch_size=1):
     pd = []
     for start in range(0, len(aligned_images), batch_size):
         pd.append(model.predict_on_batch(
-            aligned_images[start:start+batch_size]
+            aligned_images[start:start + batch_size]
         ))
     embs = l2_normalize(np.concatenate(pd))
 
