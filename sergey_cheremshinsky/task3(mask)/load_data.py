5e8426aa64b11abe859433e41ee8ffb833395fc5
sergTch <42450404+sergTch@users.noreply.github.com>
2021-02-09 02:15:45 +0200

    Add files via upload

--- sergey_cheremshinsky/task3(mask)/load_data.py
+++ sergey_cheremshinsky/task3(mask)/load_data.py
@@ -2,10 +2,11 @@ import numpy as np
 import os
 import cv2
 from random import shuffle
+from sklearn.model_selection import train_test_split
 from tqdm import tqdm 
 
 
-def label_img(name):
+def lable_img(name):
     if "out" in name:
         return np.array([0, 1])
     else:
@@ -23,35 +24,31 @@ def load_data():
         y_test = np.load('y_test.npy', allow_pickle=True)
 
         return (x_train, x_test, y_train, y_test)
-    except Exception:
+    except FileNotFoundError:
         pass
 
     indexes = []
     images = []
-    labels = []
+    lables = []
     ind = 0
     for d in dirs:
         for img in tqdm(os.listdir(d)):
-            label = label_img(img)
+            lable = lable_img(img)
             path = os.path.join(d, img)
             img = cv2.resize(cv2.imread(path), (size, size))
             images.append(np.array(img))
-            labels.append(label)
+            lables.append(lable)
             indexes.append(ind)
             ind += 1
 
     shuffle(indexes)
     n = len(images) * 4 // 5
 
-    x_train = [images[i] for i in indexes[:n]]
-    x_test = [images[i] for i in indexes[n:]]
-
-    y_train = [labels[i] for i in indexes[:n]]
-    y_test = [labels[i] for i in indexes[n:]]
+    x_train, x_test, y_train, y_test = train_test_split(images, lables, test_size=0.2)
 
     np.save('x_train.npy', x_train)
     np.save('x_test.npy', x_test)
     np.save('y_train.npy', y_train)
     np.save('y_test.npy', y_test)
 
-    return (x_train, x_test, y_train, y_test)
+    return load_data()
--- sergey_cheremshinsky/task3(mask)/main.py
+++ sergey_cheremshinsky/task3(mask)/main.py
@@ -1,30 +1,37 @@
+import tensorflow as tf
 from load_data import load_data
 from model import build_model
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 from tensorflow.keras.optimizers import SGD
 
-if __name__ == "__main__":
+def train():
     x_train, x_test, y_train, y_test = load_data()
+    # print(x_train.shape)
+    # print(x_test.shape)
+    # print(y_train.shape)
+    # print(y_test.shape)
+    # exit()
     model = build_model()
     size = len(model.layers)
     steps = 3
     batch = 10
+    epochs_per_train = 10
 
     earlystop = EarlyStopping(patience=3, monitor='accuracy')
     learning_rate_reduction = ReduceLROnPlateau(monitor='accuracy', patience=2, verbose=1, factor=0.5, min_lr=0.0001)
     callbacks = [earlystop, learning_rate_reduction]
 
-    for step in range(2, steps - 1):
+    for step in range(steps - 1):
         for i in range(size):
             model.layers[i].trainable = size - size / steps * step > i and size - size / steps * (step + 1) >= i
             model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
             history = model.fit(
                 x=x_train, 
                 y=y_train,
-                epochs=10,
+                epochs=epochs_per_train,
                 validation_data=(x_test, y_test),
-                validation_steps=1511 // batch,
-                steps_per_epoch=6042 // batch,
+                validation_steps=len(x_test) // batch,
+                steps_per_epoch=len(x_train) // batch,
                 callbacks=callbacks)
             model.save("model" + str(step))
 
@@ -36,12 +43,23 @@ if __name__ == "__main__":
     history = model.fit(
         x=x_train, 
         y=y_train,
-        epochs=10,
+        epochs=epochs_per_train,
         validation_data=(x_test, y_test),
-        validation_steps=1511 // batch,
-        steps_per_epoch=6042 // batch,
+        validation_steps=len(x_test) // batch,
+        steps_per_epoch=len(x_train) // batch,
         callbacks=callbacks)
 
     model.save("final_model")
     print(history)
     print(model.summary())
+
+def evaluate(name):
+    model = tf.keras.models.load_model(name)
+    print(len(model.layers))
+    x_train, x_test, y_train, y_test = load_data()
+    print("Evaluate on test data")
+    results = model.evaluate(x_test, y_test)
+    print("test loss, test acc:", results)
+
+if __name__ == "__main__":
+    train()
